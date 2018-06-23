a51f1006990e9747ebb23453cf95c24ab8ce7ce3
alibaseit <alibase@gmail.com>
2018-06-23 22:31:45 +0100

    Updated menu options to constants

--- src/main/java/com/principleglobal/vendingmachine/Main.java
+++ src/main/java/com/principleglobal/vendingmachine/Main.java
@@ -1,35 +1,38 @@
 package com.principleglobal.vendingmachine;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Main {
+
     private static Scanner scanner = new Scanner(System.in);
+    private static final int OPTIMAL_CHANGE = 1;
+    private static final int CHANGE = 2;
+    private static final int EXIT = 3;
+    private static int option = EXIT;
 
     public static void main(String[] args) {
         VendingMachine vendingMachine = new VendingMachine();
-        int option = showOptionMenu();
+        showOptionMenu();
         System.out.println("Enter cents: ");
         int cents = scanner.nextInt();
-        if (option == 1)
+        if (option == OPTIMAL_CHANGE)
             System.out.println(vendingMachine.getOptimalChangeFor(cents));
-        else if (option == 2)
+        else if (option == CHANGE)
             System.out.println(vendingMachine.getChangeFor(cents));
     }
 
-    private static int showOptionMenu() {
-        int option = 3;
+    private static void showOptionMenu() {
         boolean isOptionValid = false;
         while (!isOptionValid) {
             System.out.println("Please enter a valid option");
-            System.out.println("Enter option: ");
             System.out.println("1. getOptimalChange ");
             System.out.println("2. getChange ");
             System.out.println("3. Exit");
             option = scanner.nextInt();
-            if (option == 3)
+            if (option == EXIT)
                 System.exit(0);
-            isOptionValid = (option == 1 || option == 2);
+            isOptionValid = (option == OPTIMAL_CHANGE || option == CHANGE);
         }
-        return option;
     }
 }
